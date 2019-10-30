ce8661a1fc3fcab404edf32e0e4ba99188b34a3c
Lenon Mauer <DATA52858@datainfo.inf.br>
2019-10-30 08:36:20 -0300

    add colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,20 +1,20 @@
 export default {
-  white: '#FFF',
-  lighter: '#f7f7f7',
-  light: '#DDD',
+  white: '#fff',
+  lighter: '#dadada',
+  light: '#bbb',
   regular: '#999',
   dark: '#666',
-  darker: '#333',
+  darker: '#252525',
   black: '#000',
 
   primary: {
     color: '#07393C',
-    contrast: '#FaFaFa',
+    contrast: '#fafafa',
   },
 
   danger: '#B3001B',
 
   transparent: 'transparent',
-  darkTransparent: 'rgba(0, 0, 0, 0.6)',
+  darkTransparent: 'rgba(0, 0, 0, 0.3)',
   whiteTransparent: 'rgba(255, 255, 255, 0.3)',
 };
